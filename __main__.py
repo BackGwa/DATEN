d4adf379aaa5a96df568c0da8c859e5bdfb2579a
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-11-17 23:27:45 +0900

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 
+import datenlib
+
 
 def main():
-    print("\x1B[H\x1B[J")
+    print('\x1B[H\x1B[J')
     return_value = db_start()
     return 0 if(return_value == 0) else return_value
 
@@ -11,18 +13,13 @@ def main():
 def db_start():
     isCES = False
     
-    print(f"DATEN database management software\n{daten('channel')} | {daten('version')}\n")
+    print(f"DATEN database management software\n{datenlib.daten('channel')} | {datenlib.daten('version')}\n")
     while(not isCES):
         DecodeCMD = command_asked()
-        print(f'COMMAND : {DecodeCMD}')
+        result = datenlib.PARSIGN((DecodeCMD.upper()).replace(';', ''))
+        print(result if(result != '') else '')
 
     return 0
-
-
-def daten(value):
-    case = {'version' : 'v0.0',
-            'channel' : 'Development'}
-    return case[value]
 
 
 def create_blank(linelen, select_name = 0):
@@ -45,7 +42,6 @@ def command_asked():
             NewLine_input = input(f'{linelen} ↪ ')
             if(NewLine_input != ''):
                 UserCMD += ' ' + NewLine_input if(NewLine_input != ';') else NewLine_input
-    
     return UserCMD
 
 
