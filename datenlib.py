4f36987015ef1f7ce8d9b94771f768fed6023b2e
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-11-18 10:07:57 +0900

    Add Features & improvement & bug fixs
    [Add Features]
    - Viewing the list of databases
    - Selecting and unselecting database
    
    [improvement]
    - code visibility
    - Help Command
    
    [bug fixs]
    - Fixed a bug that occurred when a factor did not exist in 'DATA'.

--- datenlib.py
+++ datenlib.py
@@ -2,16 +2,22 @@
 #-*- coding:utf-8 -*-
 
 import os
+import __main__ as cr
 
 COLOR_RED = '\033[91m'
 COLOR_GREEN = '\033[92m'
 COLOR_YELLOW = '\033[93m'
 COLOR_END = '\033[0m'
 
+
+global now_select
+now_select = ''
+
 def daten(value):
     case = {'version' : 'v0.0',
             'channel' : 'Development'}
     return case[value]
+
 
 def isHELP(command):
     commandlist = command.split(' ')
@@ -30,39 +36,19 @@ def isINFO(command):
 def isQUIT(command):
     return True if(command == 'QUIT' or command == 'EXIT') else False
 
+
 def PARSIGN(command):
     result = ''
     
     while(True):
         if(isHELP(command)):
             if(command == 'HELP'):
-                print('''
-                    
-=============================[ List of DATEN commands ]=============================
-
-    HELP {VALUE}        =>  Outputs all commands.
-    DATA {MODE} {VALUE} =>  Database management commands.
-    CLEAR               =>  Clears all content on the current screen.
-    INFO                =>  Outputs information from the current DATEN software.
-    QUIT & EXIT         =>  Shutdown the DATEN software.
-
-====================================================================================
-
-''')
+                print(help_str('all'))
                 break
             else:
                 commandlist = command.split(' ')
                 if(commandlist[1] == 'DATA'):
-                    print('''
-                          
-=============================[ Commands for DATA ]=============================
-
-    CREATE {FILE_NAME}  =>  Create a new database.
-    REMOVE {FILE_NAME}  =>  Removes an existing database.
-
-===============================================================================
-                          
-''')
+                    print(help_str('data'))
                 else:
                     print(syntaxerror(command, 'uhl'))
                 break
@@ -70,19 +56,52 @@ def PARSIGN(command):
         elif(isDATA(command)):
             commandlist = command.split(' ', 2)
             
-            if(commandlist[1] == 'CREATE'):
-                if(len(commandlist) >= 3):
-                    create_data(commandlist[2])
-                else:
-                    result = syntaxerror(command, 'dce')
-                break
+            if(len(commandlist) >= 2):
+                if(commandlist[1] == 'CREATE'):
+                    if(len(commandlist) >= 3):
+                        create_data(commandlist[2])
+                    else:
+                        result = syntaxerror(command, 'dce')
+                    break
+                
+                elif(commandlist[1] == 'REMOVE'):
+                    if(len(commandlist) >= 3):
+                        remove_data(commandlist[2])
+                    else:
+                        result = syntaxerror(command, 'dce')
+                    break
+                
+                elif(commandlist[1] == 'LIST'):
+                    index = 0
+                    get_datalist = datalist()
+                    print(f'\n{line_creater(10)}[ List of databases ]{line_creater(10)}\n')
+                    if(len(get_datalist) != 0):
+                        for value in get_datalist:
+                            index += 1
+                            print(f'    [{index}] {value}')
+                    else:
+                        print(richtext('    [!] No databases currently exist.', 'YELLOW'))
+                    print(f'\n{line_creater(21 + 20)}\n')
+                    break
             
-            elif(commandlist[1] == 'REMOVE'):
-                if(len(commandlist) >= 3):
-                    remove_data(commandlist[2])
+                elif(commandlist[1] == 'SELECT'):
+                    if(len(commandlist) >= 3):
+                        data_select(commandlist[2])
+                    else:
+                        result = syntaxerror(command, 'dce')
+                    break
+                
+                elif(commandlist[1] == 'UNSELECT'):
+                    if(not len(commandlist) >= 3):
+                        data_unselect()
+                    else:
+                        result = syntaxerror(command, 'dce')
+                    break
+                
                 else:
-                    result = syntaxerror(command, 'dce')
-                break
+                    print(syntaxerror(command, 'ukn'))
+                    break
+                
             else:
                 result = syntaxerror(command, 'ukn')
                 break
@@ -108,6 +127,7 @@ def PARSIGN(command):
             
     return result
 
+
 def create_data(name):
     if((name[len(name)-1:len(name)] == ' ')):
         print(syntaxerror(name, 'des'))
@@ -124,6 +144,32 @@ def create_data(name):
             if(returnvalue):
                 print(richtext(f'↪ Successfully created a database with \'{undername}\'!', 'GREEN'))
     return None
+
+
+def data_select(name):
+    if((name[len(name)-1:len(name)] == ' ')):
+        print(syntaxerror(name, 'des'))
+    else:
+        undername = name.replace(' ', '_')
+        if(name[0:1].isdigit()):
+            print(syntaxerror(name, 'den'))
+        elif(name == ''):
+            print(syntaxerror(name, 'dbb'))
+        elif(not os.path.isfile(f'{undername}.dt')):
+            print(syntaxerror(name, 'ukf'))
+        else:
+            global now_select
+            now_select = undername
+            cr.select = now_select
+    return None
+
+
+def data_unselect():
+    global now_select
+    now_select = ''
+    cr.select = now_select
+    return None
+
 
 def remove_data(name):
     if((name[len(name)-1:len(name)] == ' ')):
@@ -142,6 +188,11 @@ def remove_data(name):
                 print(richtext(f'↪ Successfully removed the database at \'{undername}\'!', 'GREEN'))
     return None
 
+
+def datalist():
+    return [file for file in os.listdir(os.getcwd()) if file.endswith('.dt')]
+
+
 def syntaxerror(command, errcode):
     case = {'ukn' : f'{command}\nThis type of command is not supported!',
             'dce' : f'{command}\nThe name of the database for executing the command is not defined!',
@@ -152,9 +203,11 @@ def syntaxerror(command, errcode):
             'ukf' : f'\'{command}\'\nThis database file does not exist!',
             'dtu' : f'\'{command}\'\nThis database file already exists!',
             'dbb' : f'It is impossible to leave the database name blank!',
-            'uhl' : f'{command}\nThe following help does not exist!'}
+            'uhl' : f'{command}\nThe following help does not exist!',
+            'apr' : f'{command}\nAdditional factor not verified exist! These additional factor are not allowed!'}
     
-    return richtext('\n============================================================\n' + '↪ ' + case[errcode] + '\n============================================================', 'RED')
+    return richtext(f'\n{line_creater(60)}\n' + '↪ ' + case[errcode] + f'\n{line_creater(60)}', 'RED')
+
 
 def richtext(text, color):
     if(color == 'RED'):
@@ -163,7 +216,15 @@ def richtext(text, color):
         return COLOR_YELLOW + text + COLOR_END
     elif(color == 'GREEN'):
         return COLOR_GREEN + text + COLOR_END
-    
+
+
+def line_creater(value):
+    result = ''
+    for i in range(0, value):
+        result += '='
+    return result
+
+
 def file_manage_system(file_mode, file_name):
     
     file_path = f'{file_name}.dt'
@@ -185,3 +246,32 @@ def file_manage_system(file_mode, file_name):
         except:
             print(syntaxerror(file_name, 'fre'))
             return False
+        
+        
+def help_str(value):
+    if(value == 'all'):
+        return (f'''
+                    
+{line_creater(30)}[ List of DATEN commands ]{line_creater(30)}
+
+    HELP [VALUE]        =>  Outputs all commands.
+    DATA [MODE] [VALUE] =>  Database management commands.
+    CLEAR               =>  Clears all content on the current screen.
+    INFO                =>  Outputs information from the current DATEN software.
+    QUIT & EXIT         =>  Shutdown the DATEN software.
+
+{line_creater(26 + 60)}
+''')
+    elif(value == 'data'):
+        return (f'''
+                          
+{line_creater(30)}[ Commands for DATA ]{line_creater(30)}
+
+    CREATE [FILE_NAME]  =>  Create a new database.
+    REMOVE [FILE_NAME]  =>  Removes an existing database.
+    SELECT [FILE_NAME]  =>  Select the database.
+    UNSELECT            =>  unselect the database.
+    LIST                =>  Returns a list of all data.
+
+{line_creater(21 + 60)}
+''')
